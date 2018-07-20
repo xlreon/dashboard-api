5c237dcf79dae1e459d66c75b5b8545ce55f66a6
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2018-07-21 02:25:46 +0530

    Code clean up. :sunglasses:

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 var  express = require('express')
 var FCM = require('fcm-node')
-var serverKey = "AAAAtq-PMqs:APA91bEgY4eGZZU5vf8cTFRkiUUnnAq7sRL9owz8170x-tuv8-cSxQKQ3RPc0aIsdMal5H_yKABZVcl681hN_oSs_k6YpXncRnsltUxoKgtBZAZmtEqtAzBt-RCFLzywPn-SusLlAD0hsnYR4vmGP8tG3rbuq-EETw"
+var serverKey = require('./serverKey.json')
 var fcm = new FCM(serverKey)
 var easyPbkdf2 = require("easy-pbkdf2")();
 var salt = easyPbkdf2.generateSalt();
 var bodyParser = require("body-parser");
-
+var deviceToken = require('./token.json')
 
 var app = express()
 
 app.use(bodyParser.urlencoded({
     extended: true
- }));
-//  app.use(bodyParser.json());
+}));
 
 app.get('/',(req,res) => {
     res.send('hello world')
@@ -24,13 +23,7 @@ app.get("/test",(req,res) => {
     console.log(req.query.token)
 
     var message = {
-        // to: 'cIRJQ-uhjFw:APA91bEQF6o29nvidjgLZmprBcNN-VTS9woYWyAZeQ7bgY7AKQHTjBCptk3yacK-phTp4_VXtwULRUvosWSRy03wT7uv2lB6mm0N_Sv4je7S3b9br3YQdi5DW4nND5gbvfDjY6vvTwQcWuU5j9Y2c2qFCfNieg6FpQ',
         to: req.query.token,
-        // message_id: "ajsdhak",
-        // priority: "high",
-        // data: {
-        //     command: "gps"
-        // }, 
         notification: {
             title: "Test",
             body: "Test"
@@ -57,9 +50,7 @@ app.post('/register',(req,res)=>{
     var password = req.body.password
     var imei = req.body.imei
     var hashedPassword = password
-    // var password = "abcd"
-    
-    
+
     callback = ( err, passwordHash, originalSalt ) => { 
         hashedPassword=passwordHash
         console.log(name+"\n")
@@ -88,15 +79,6 @@ app.post('/login',(req,res)=> {
 
     easyPbkdf2.secureHash(password, salt, callback)
 })
-
-// featureList = {
-//     location: "/feature/location",
-//     wipe: "/feature/wipe",
-//     alarmOn: "/feature/alarm/on",
-//     alarmOff: "/feature/alarm/off",
-//     setRemotePassword: "/feature/setremotepassword",
-//     lock: "/feature/lock"
-// }
 
 var commands = {
     "wipe": {
@@ -135,10 +117,20 @@ getCommand = (commandName,token) => {
             }
         }               
 
-    app.post("/feature",(req,res) => {
-        var featureName = req.body.featureName
-        console.log("Current feature -> ",featureName)
-        console.log(getCommand(featureName,"SET TOKEN ID HERE"))
+app.post("/feature",(req,res) => {
+    var featureName = req.body.featureName
+    console.log("Current feature -> ",featureName)
+    var message = getCommand(featureName,deviceToken)
+    fcm.send(message, (err,response) => {
+        if(err) {
+            console.log('Error')
+        }
+        else {
+            console.log('Notidication sent to token id')
+            console.log(JSON.parse(response))
+        }
     })
+})
+
 
 app.listen("8080")
