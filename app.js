41da5e47bbfd7d2d9577d2083a7ca774c3b2398c
Md Azmal <azmalmd12@gmail.com>
2018-07-21 10:09:42 +0530

    Added localdb for login

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var  express = require('express')
+var express = require('express')
 var FCM = require('fcm-node')
 var serverKey = require('./serverKey.json')
 var fcm = new FCM(serverKey)
@@ -6,19 +6,23 @@ var easyPbkdf2 = require("easy-pbkdf2")();
 var salt = easyPbkdf2.generateSalt();
 var bodyParser = require("body-parser");
 var deviceToken = require('./token.json')
+var mongoose = require('mongoose')
+var User = require('./models/user.js')
 
 var app = express()
+
+mongoose.connect('mongodb://localhost:27017/dashboard', { useNewUrlParser: true })
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.send('hello world')
 
 })
 
-app.get("/test",(req,res) => {
+app.get("/test", (req, res) => {
 
     console.log(req.query.token)
 
@@ -31,8 +35,8 @@ app.get("/test",(req,res) => {
 
     }
 
-    fcm.send(message, (err,response) => {
-        if(err) {
+    fcm.send(message, (err, response) => {
+        if (err) {
             console.log('Error')
         }
         else {
@@ -42,8 +46,8 @@ app.get("/test",(req,res) => {
     })
 })
 
-app.post('/register',(req,res)=>{
-    
+app.post('/register', (req, res) => {
+
     var name = req.body.name
     var email = req.body.email
     var token = req.body.token
@@ -51,30 +55,59 @@ app.post('/register',(req,res)=>{
     var imei = req.body.imei
     var hashedPassword = password
 
-    callback = ( err, passwordHash, originalSalt ) => { 
-        hashedPassword=passwordHash
-        console.log(name+"\n")
-        console.log(email+"\n")
-        console.log(token+"\n")
-        console.log(imei+"\n")
-        console.log(hashedPassword+"\n")
+    callback = (err, passwordHash, originalSalt) => {
+        hashedPassword = passwordHash
+        // console.log(name + "\n")
+        // console.log(email + "\n")
+        // console.log(token + "\n")
+        // console.log(imei + "\n")
+        // console.log(hashedPassword + "\n")
+        var newUser = {
+            name: req.body.name,
+            email: req.body.email,
+            token: req.body.token,
+            imei: req.body.imei,
+            hashedPassword: hashedPassword
+        }
+
+        User.create(newUser, (err, users) => {
+            if (!err) {
+                console.log('______USER DATA_________')
+                console.log(users)
+            } else {
+                console.log('Error : ' + err)
+            }
+        })
     }
-    
-    easyPbkdf2.secureHash( password, salt, callback)
+
+    easyPbkdf2.secureHash(password, salt, callback)
 })
 
-app.post('/login',(req,res)=> {
+app.post('/login', (req, res) => {
     var email = req.body.email
     var password = req.body.password
     var imei = req.body.imei
     var hashedPassword = password
 
-    callback = (err, passwordHash, originalSalt ) => {
+    callback = (err, passwordHash, originalSalt) => {
         hashedPassword = passwordHash
-        console.log(email+"\n")
-        console.log(password+"\n")
-        console.log(imei+"\n")
-        console.log(hashedPassword+"\n")
+        // console.log(email + "\n")
+        // console.log(password + "\n")
+        // console.log(imei + "\n")
+        // console.log(hashedPassword + "\n")
+
+        User.findOne({ email: req.body.email, hashedPassword: hashedPassword }, (err, users) => {
+            if (!err) {
+                console.log('_____login data______')
+                if (users) {
+                    console.log(users)
+                } else {
+                    console.log('Invalid Cridentials')
+                }
+            } else {
+                console.log('Error : ' + err)
+            }
+        })
     }
 
     easyPbkdf2.secureHash(password, salt, callback)
@@ -84,15 +117,15 @@ var commands = {
     "wipe": {
         "command": "wipe"
     },
-    "alarmOn" : {
+    "alarmOn": {
         "command": "alarm",
         "action": "on"
     },
-    "alarmOff" : {
+    "alarmOff": {
         "command": "alarm",
         "action": "off"
     },
-    "setRemotePassword" : {
+    "setRemotePassword": {
         "command": "password",
         "pass": "1234",
         "custom": "true", // true or false depending on the message
@@ -109,20 +142,20 @@ var commands = {
 
 
 
-getCommand = (commandName,token) => {
-        return {
-            to: token,
-            priority: "high",
-            data: commands[commandName]
-            }
-        }               
+getCommand = (commandName, token) => {
+    return {
+        to: token,
+        priority: "high",
+        data: commands[commandName]
+    }
+}
 
-app.post("/feature",(req,res) => {
+app.post("/feature", (req, res) => {
     var featureName = req.body.featureName
-    console.log("Current feature -> ",featureName)
-    var message = getCommand(featureName,deviceToken)
-    fcm.send(message, (err,response) => {
-        if(err) {
+    console.log("Current feature -> ", featureName)
+    var message = getCommand(featureName, deviceToken)
+    fcm.send(message, (err, response) => {
+        if (err) {
             console.log('Error')
         }
         else {
@@ -131,6 +164,5 @@ app.post("/feature",(req,res) => {
         }
     })
 })
-
 
 app.listen("8080")
