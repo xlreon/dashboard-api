b6b01e154f361dac22274f78d06604496ae038a1
Md Azmal <azmalmd12@gmail.com>
2018-07-22 08:43:35 +0530

    updated schema for device infos

--- models/meta.js
+++ models/meta.js
@@ -2,7 +2,15 @@ var mongoose = require('mongoose')
 
 var mobileinfo = mongoose.Schema({
     imei: String,
-    token: String
+    token: String,
+    device: {
+        deviceDetails: {
+            os: String,
+            battery: Number,
+            wifi: String
+        },
+        features: [{ name: String, description: String }]
+    }
 })
 
 module.exports = mongoose.model('Mobileinfo', mobileinfo)
