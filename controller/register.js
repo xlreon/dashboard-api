241c1356ef847ddf8c68f5319fbd8fc7f9c26d71
Md Azmal <azmalmd12@gmail.com>
2018-07-24 02:31:53 +0530

    updated check for user exist

--- controller/register.js
+++ controller/register.js
@@ -17,50 +17,76 @@ router.post('/register', (req, res) => {
     callback = (err, passwordHash, originalSalt) => {
         hashedPassword = passwordHash
 
-        Mobileinfo.create({ imei: req.body.imei, token: req.body.token }, (err, meta) => {
-            if (!err) {
-                var newUser = {
-                    name: req.body.name,
-                    email: req.body.email,
-                    hashedPassword: hashedPassword,
-                    salt: salt,
-                    mobileinfos: meta
-                }
-                User.create(newUser, (err, user) => {
-                    if (!err) {
-                        console.log('______USER DATA_________')
-                        console.log(user)
-                        response = {
-                            status: 1,
-                            body: {
-                                info: "user successfully registered",
-                                error: null,
-                                content: null
-                            }
-                        }
-                        res.send(response)
-                    } else {
-                        response = {
-                            status: -2,
-                            body: {
-                                info: "user db eroor",
-                                error: err,
-                                content: null
-                            }
-                        }
-                        res.send(response)
-                    }
-                })
-            } else {
+        User.findOne({ email: req.body.email }, (err, userext) => {
+            if (!err && userext) {
                 response = {
-                    status: -3,
+                    status: 9,
                     body: {
-                        info: "imei and token db eroor",
-                        error: err,
+                        info: "user exists",
+                        error: null,
                         content: null
                     }
                 }
                 res.send(response)
+            } else {
+                if (!userext) {
+                    Mobileinfo.create({ imei: req.body.imei, token: req.body.token }, (err, meta) => {
+                        if (!err) {
+                            var newUser = {
+                                name: req.body.name,
+                                email: req.body.email,
+                                hashedPassword: hashedPassword,
+                                salt: salt,
+                                mobileinfos: meta
+                            }
+                            User.create(newUser, (err, user) => {
+                                if (!err) {
+                                    console.log('______USER DATA_________')
+                                    console.log(user)
+                                    response = {
+                                        status: 1,
+                                        body: {
+                                            info: "user successfully registered",
+                                            error: null,
+                                            content: null
+                                        }
+                                    }
+                                    res.send(response)
+                                } else {
+                                    response = {
+                                        status: -2,
+                                        body: {
+                                            info: "user db eroor",
+                                            error: err,
+                                            content: null
+                                        }
+                                    }
+                                    res.send(response)
+                                }
+                            })
+                        } else {
+                            response = {
+                                status: -3,
+                                body: {
+                                    info: "imei and token db eroor",
+                                    error: err,
+                                    content: null
+                                }
+                            }
+                            res.send(response)
+                        }
+                    })
+                } else {
+                    response = {
+                        status: -2,
+                        body: {
+                            info: "user db eroor",
+                            error: err,
+                            content: null
+                        }
+                    }
+                    res.send(response)
+                }
             }
         })
     }
