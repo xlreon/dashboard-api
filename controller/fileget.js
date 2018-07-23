faabbe27020c3242b7446ef3dc4de6a0e85c650b
Md Azmal <azmalmd12@gmail.com>
2018-07-24 02:09:08 +0530

    get file from aws s3

---
+++ controller/fileget.js
@@ -0,0 +1,43 @@
+var express = require('express')
+var router = express.Router();
+var AWS = require('aws-sdk')
+var bodyParser = require("body-parser")
+
+router.use(bodyParser.urlencoded({ extended: true }))
+
+var aws_access = require('../keys/configBucket.json')
+var bucketName = require('../keys/bucketName.json')
+
+// aws configuration
+AWS.config.update(aws_access)
+var s3 = new AWS.S3()
+// downloads data from aws cloud
+// recieves key (of db file) and name (of db file)
+router.post('/file/get', (req, res) => {
+    s3.getObject({ Bucket: bucketName, Key: `${req.body.key}/${req.body.name}` }, (err, file) => {
+        if (!err) {
+            console.log(file)
+            response = {
+                status: 8,
+                body: {
+                    info: "user data downloaded successfully from aws s3",
+                    error: null,
+                    content: file
+                }
+            }
+            res.send(response)
+        } else {
+            response = {
+                status: -11,
+                body: {
+                    info: "aws s3 error",
+                    error: err,
+                    content: null
+                }
+            }
+            res.send(response)
+        }
+    })
+})
+
+module.exports = router
