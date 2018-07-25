5cc3b9d50bce95ed0f41ee6f449c23a173241634
Md Azmal <azmalmd12@gmail.com>
2018-07-26 02:16:50 +0530

    bug fixes

--- controller/verify.js
+++ controller/verify.js
@@ -4,18 +4,9 @@ var User = require('../models/user')
 
 router.get('/:id', (req, res) => {
     var response = {}
-
     User.findOneAndUpdate({ tmpsalt: req.params.id }, { $set: { 'verified': true } }, (err, user) => {
         if (!err && user) {
-            response = {
-                status: 10,
-                body: {
-                    info: "email verified",
-                    error: null,
-                    content: null
-                }
-            }
-            res.send(response)
+            res.send(`<p>Your Email Has Been Verified</p>`)
         } else {
             if (!user) {
                 response = {
