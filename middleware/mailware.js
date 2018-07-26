3c6efe6206eb4f198db0d3f20d47d65f29cf82c5
Md Azmal <azmalmd12@gmail.com>
2018-07-27 00:28:34 +0530

    added middleware to check if mail sent was successful

---
+++ middleware/mailware.js
@@ -0,0 +1,42 @@
+var nodemailer = require('nodemailer')
+var sesTransport = require('nodemailer-ses-transport')
+var host_url = 'localhost:8080'
+var easyPbkdf2 = require("easy-pbkdf2")()
+var tmpsalt = easyPbkdf2.generateSalt()
+var config = require('../keys/configBucket.json')
+
+function mailware(req, res, next) {
+    var transporter = nodemailer.createTransport(sesTransport({
+        accessKeyId: config.accessKeyId,
+        secretAccessKey: config.secretAccessKey,
+        sendingRate: 5
+    }));
+
+    var mailOptions = {
+        from: config.email,
+        to: req.body.email,
+        subject: 'Verification of email',
+        text: 'please verify your email',
+        html: `<h1>${host_url}/${tmpsalt}</h1> <br> <h2>Copy the above link and open in new tab to verify</h2>`
+    };
+
+    transporter.sendMail(mailOptions, function (err, info) {
+        if (err) {
+            response = {
+                status: -17,
+                body: {
+                    info: "nodemailer smtp error",
+                    error: err,
+                    content: null
+                }
+            }
+            res.send(response)
+        } else {
+            console.log('Email sent: ' + info.response);
+            req.tmpsalt = tmpsalt
+            next()
+        }
+    });
+}
+
+module.exports = mailware
