f61c5d58782e68eb4bc5c05a8f394d13495fde65
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2018-07-29 16:14:28 +0530

    Mobile info schema modified.

--- models/mobileinfo.js
+++ models/mobileinfo.js
@@ -3,16 +3,22 @@ var mongoose = require('mongoose')
 var mobileinfo = mongoose.Schema({
     imei: Number,
     token: String,
-    device: {
-        os: String,
-        battery: Number,
-        wifi: String,
-        name : String,
-        location : {
-            lat : Number,
-            lng : Number
-        }
-    },
+    ssid: String,
+    battery: String,
+    brand: String,
+    model: String,
+    gps: String,
+    data: String,
+    // device: {
+    //     os: String,
+    //     battery: Number,
+    //     wifi: String,
+    //     name : String,
+    //     location : {
+    //         lat : Number,
+    //         lng : Number
+    //     }
+    // },
     files: [{
         key: String,
         name: String
